39909aeec364ba883ba4013f0a0661d59c235308
David Reina <davidereina@hotmail.com>
2023-10-05 15:42:34 -0500

    Program that checks if the value entered for the temperature is valid, and then tanking it into account print out a message in the console

---
+++ ChallengeIfEnhaced/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ChallengeIfEnhaced
+{
+    internal class Program
+    {
+        public static bool isInteger;
+        public static int numberInt;
+        public static string result = "";
+        static void CheckTemperature(string number)
+        {
+            isInteger = Int32.TryParse(number, out numberInt);
+            if (!isInteger)
+            {
+                result = "Not a valid Temperature.";
+            }
+            else
+            {
+                result = numberInt <= 15 ? "it is too cold here" : numberInt >= 16 && numberInt <= 28 ? "it is ok" : "it is hot here";
+            }
+            Console.WriteLine(result);
+        }
+        static void Main(string[] args)
+        {
+            CheckTemperature("15");
+        }
+    }
+}
